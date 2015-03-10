3204c78b82e19d8780492ef213572dd0e777b36b
Michael <michael@ourlaborisjoy.com>
2015-03-10 11:42:36 +0100

    assignment init

---
+++ angularjs/assets/js/app.js
@@ -0,0 +1,43 @@
+(function(exports) {
+    angular.module("ngAppAssignment", []).controller("assignmentController", [ "$scope", function($scope) {
+        $scope.user = {
+            firstname: "Your firstname",
+            lastname: "Your lastname",
+            city: "Your city",
+            website: "www.domain.com",
+            cv: "www.domain.com/cv.pdf"
+        };
+        $scope.list = {
+            "1": {
+                title: "Vestas A/S",
+                desc: "Frontend developer",
+                year: 2015
+            },
+            "2": {
+                title: "K&oslash;benhavn Kommune",
+                desc: "Frontend developer",
+                year: 2014
+            },
+            "3": {
+                title: "K&oslash;benhavn Kommune",
+                desc: "Frontend developer",
+                year: 2013
+            }
+        };
+        $scope.filteredList = $scope.list;
+        function options() {
+            $scope.yearsList = _.uniq(_.pluck($scope.list, "year"));
+            console.log($scope.yearsList);
+        }
+        options();
+        $scope.updateList = function(year) {
+            if (year) {
+                $scope.filteredList = _.where($scope.list, {
+                    year: year
+                });
+            } else {
+                $scope.filteredList = $scope.list;
+            }
+        };
+    } ]);
+})(window);
---
+++ html/gruntfile.js
@@ -0,0 +1,107 @@
+module.exports = function(grunt) {
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+        
+        connect: {
+            server: {
+                options: {
+                    port: 1337,
+                    keepalive: true,
+                    base: '',
+                    hostname: 'localhost',
+                    open: true,
+                    index: 'index.html'
+                }
+            }
+        },
+
+        //
+        // watch for changes
+        //
+        watch: {
+            options: {
+                atBegin: true
+            },
+            sass: {
+                files: './source/sass/**/*.{scss,sass}',
+                tasks: ['compass:watch']
+            },
+            js: {
+                files: './source/js/app.js',
+                tasks: ['uglify:watch']
+            }
+        },
+
+
+        //
+        // sass compilation with compass
+        //
+        compass: {
+            options: {
+                sassDir: 'source/sass',
+                cssDir: 'assets/css',
+                imagesDir: 'assets/images',
+                specify: ['source/sass/screen.scss'],
+                relativeAssets: true,
+                noLineComments: true,
+                bundleExec: true
+            },
+            watch: {
+                options: {
+                    environment: 'development',
+                    noLineComments: false
+                }
+            },
+            build: {
+                options: {
+                    outputStyle: 'compressed',
+                    environment: 'production',
+                    noLineComments: true
+                }
+            }
+        },
+
+        uglify: {
+            watch: {
+                files: {
+                    'assets/js/app.js': ['./source/js/app.js']
+                },
+                options: {
+                    compress: false,
+                    mangle: false,
+                    beautify: true
+                }
+            },
+            build: {
+                files: {
+                    'assets/js/app.js': ['./source/js/app.js']
+                },
+                options: {
+                    preserveComments: false,
+                    mangle: true,
+                    compress: {
+                        drop_console: true
+                    }
+                }
+            }
+        }
+
+    });
+
+
+    //
+    // Use grunt-tasks to load modules instead of
+    // grunt.loadNpmTasks('xxx');
+    //
+    require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['compass:build', 'uglify:build']);
+
+    grunt.registerTask('server', function (target) {
+        grunt.task.run([
+            'connect'
+        ]);
+    });
+};
